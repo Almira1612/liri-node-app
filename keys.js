c0f9fbf05f248d6147b43f0ef5e6a15edca9769b
Almira1612 <almira1612@gmail.com>
2017-08-18 22:54:50 -0400

    addFiles

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'EXiMUFUU7v6LPrGLgRIcG2Jqx',
+  consumer_secret: 'TJWlvFiuuQvL43EQRPFzURGWdIBgK4MNSuuHHZCjnhfpBqsAp9',
+  access_token_key: '895352780361265152-ewq1wAXzHyzsmWvz9AgjIGaYEbwt9Uf',
+  access_token_secret: 'Fv3c1watiE5R1zPZE95h0HJ7sVQbS7RxniaZkEpC6lGzr',
+}
---
+++ liri.js
@@ -0,0 +1,7 @@
+var inquirer = require("inquirer");
+
+var fs = require("fs");
+var keys = require("./keys.js");
+var twitter = require('twitter');
+var spotify = require('spotify');
+
