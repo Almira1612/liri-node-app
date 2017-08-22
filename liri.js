aaa04903d9ae98e22df4e59a70979eb4c4620277
Almira1612 <almira1612@gmail.com>
2017-08-21 20:54:26 -0400

    finish

--- liri.js
+++ liri.js
@@ -5,7 +5,7 @@ var request = require("request");
 var fs = require("fs");
 var keys = require("./keys.js");
 var twitter = require('twitter');
-//var spotify = require('spotify');
+var Spotify = require('node-spotify-api');
 var params = { screen_name: 'almira1612', count: 20 };
 
 
@@ -21,15 +21,44 @@ inquirer
   ])
 
 .then(function(inquirerResponse) {
+	
     // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
     if (inquirerResponse.actions === "My-tweets") {
       myTweets();
     }
     else if (inquirerResponse.actions === "Spotify-this-song"){
-     spotify();
+     inquirer
+        .prompt([
+         {
+      type: "input",
+      message: "Type the song name here: ",
+      name: "songName"
+          } ])
+
+      .then(function(userSong){
+         
+      	
+      spotify(userSong.songName);
+
+      });
+     
+    
     }
      else if (inquirerResponse.actions === "movie-this"){
-      movieThis();
+      inquirer
+        .prompt([
+         {
+      type: "input",
+      message: "Type the movie name here: ",
+      name: "movieName"
+          } ])
+
+      .then(function(userMovie){
+
+      	
+      movieThis(userMovie.movieName);
+
+      });
     }
      else if (inquirerResponse.actions === "do-what-it-says"){
       doWhatSay();
@@ -53,46 +82,93 @@ function myTweets(){
      
     }
   });
-};
+} // end myTweets function
+
 // spotify function
 
 //Client ID
 //f6b5a62182af435b8bf6aeb6efd92f31
 //Client Secret
 //6e35f47012b244288f7deb518a6e3875
-// function spotify (songName){
-// 	var songName = process.argv[3];
-// 	if (songName === undefined){
-// 		console.log("Undefined, Please try again!");
-// 	};
-//    params = songName;
-//    spotify.search({ type:"track", query: params}, function(err,data){
-//       if (!err) {
-//       	var songInfo = data.tracks.items;
-// 				for (var i = 0; i < 5; i++) {
-// 					if (songInfo[i] != undefined) {
-// 						var spotifyResults =
-// 						"Artist: " + songInfo[i].artists[0].name + "\r\n" +
-// 						"Song: " + songInfo[i].name + "\r\n" +
-// 						"Album the song is from: " + songInfo[i].album.name + "\r\n" +
-// 						"Preview Url: " + songInfo[i].preview_url + "\r\n" + 
-// 						"------------------------------ " + i + " ------------------------------" + "\r\n";
-// 						console.log(spotifyResults);
-// 						log(spotifyResults); // calling log function
-//        }
-//     }
-// }
-//    else {
-//    	console.log(err);
-//    	return;
-//    }
-//    	});
+function spotify (songName){
+  
+  if (songName === undefined){
+  	songName = "The Sign"
+  }
+	var spotify = new Spotify({
+  id: "f6b5a62182af435b8bf6aeb6efd92f31",
+  secret: "6e35f47012b244288f7deb518a6e3875"
+});
+	
+	
+	
+	if (songName == null) {
+    songName = 'The Sign';
+  };
 
+   //params = songName;
+   spotify.search({ type:"track", query: songName}, function(err,data){
+      if (!err) {
+      	
+      	var songInfo = data.tracks.items;
+				for (var i = 0; i < 5; i++) {
+					if (songInfo[i] != undefined) {
+						var spotifyResults =
+						"Artist: " + songInfo[i].artists[0].name + "\r\n" +
+						"Song: " + songInfo[i].name + "\r\n" +
+						"Album the song is from: " + songInfo[i].album.name + "\r\n" +
+						"Preview Url: " + songInfo[i].preview_url + "\r\n" + 
+						"------------------------------ " + i + " ------------------------------" + "\r\n";
+						console.log(spotifyResults);
+						 
+       }
+    }
+}
+   else {
+   	console.log(err);
+   	return;
+   }
+   	});
+} // end spotify function
 
+ // movie function
+ function movieThis(movieName){
 
-//  };
+ 	
+  var urlHit= "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+ if (movieName == null) {
+    movieName = 'Mr Nobody';
+  } 
 
-//  // movie function
-//  function movieThis(){
-//   var movie = process.argv[3];
-//  }
+ request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = [];
+      var jsonData = JSON.parse(body);
+
+      data.push({
+      'Title: ' : jsonData.Title,
+      'Year: ' : jsonData.Year,
+      'IMDB Rating: ' : jsonData.imdbRating,
+      'Rotten Tomatoes Rating: ' : jsonData.tomatoRating,
+      'Country: ' : jsonData.Country,
+      'Language: ' : jsonData.Language,
+      'Plot: ' : jsonData.Plot,
+      'Actors: ' : jsonData.Actors,
+
+  });
+      console.log(data);
+     
+}
+  });
+ } // end movieThis function
+
+function doWhatSay() {
+		fs.readFile("random.txt", "utf8", function(error, data){
+			if (!error) {
+				doWhatSayResults = data.split(",");
+				spotify(doWhatSayResults[1], doWhatSayResults[2]);
+			} else {
+				console.log("Error occurred" + error);
+			}
+		});
+	}; // end doWhatSay function
